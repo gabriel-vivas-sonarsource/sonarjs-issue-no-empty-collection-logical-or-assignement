400cd12d2b2b4fda2f1c2c404d420809b504337e
Damien Cassou <damien@cassou.me>
2024-07-26 17:17:20 +0200

    Reproduce bug

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-export function add(number1, number2) {
-  return number1 + number2;
+let strings = [];
+
+// no-empty-collection reports an issue here. This is ok because the
+// code below "expands" to `strings[1] = strings[1] || "foo"` and the
+// 2nd `strings[1]` is noncompliant:
+strings[1] ||= "foo";
+
+
+// no-empty-collection reports an issue here. This is wrong because at
+// this point strings[1] contains "foo":
+if (strings[1]) {
+  // Same here. Executing this program prints "foo" further confirming
+  // that the `if` statement above is correct:
+  console.log(strings[1]);
 }
