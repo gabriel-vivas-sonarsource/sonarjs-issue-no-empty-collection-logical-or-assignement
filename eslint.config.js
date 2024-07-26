f4b8c6fd1d308497935ee81f160a3853476210e0
Damien Cassou <damien@cassou.me>
2024-07-26 17:08:48 +0200

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import prettierRecommendedConfig from "eslint-plugin-prettier/recommended";
+import pluginJs from "@eslint/js";
+import pluginPromise from "eslint-plugin-promise";
+
+export default [
+  pluginJs.configs.recommended,
+
+  pluginPromise.configs["flat/recommended"],
+
+  // prettier should be last
+  {
+    name: "@foretagsplatsen/main prettier plugin",
+    ...prettierRecommendedConfig,
+  },
+];
