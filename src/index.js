49e972a4605ce7daa1c963ad2af3e219d70c9c64
Gabriel Vivas <103954349+gabriel-vivas-sonarsource@users.noreply.github.com>
2024-07-29 12:04:09 +0200

    Update index.js with OR default

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let strings = [];
 // no-empty-collection reports an issue here. This is ok because the
 // code below "expands" to `strings[1] = strings[1] || "foo"` and the
 // 2nd `strings[1]` is noncompliant:
-strings[0] = "foo";
+strings[0] = strings[0] || "foo";
 
 
 // no-empty-collection reports an issue here. This is wrong because at
