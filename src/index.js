50c3c1c23f431e4ad883e318fe71f454743f87b0
Gabriel Vivas <103954349+gabriel-vivas-sonarsource@users.noreply.github.com>
2024-07-29 12:00:34 +0200

    Update index.js using index 0

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ let strings = [];
 // no-empty-collection reports an issue here. This is ok because the
 // code below "expands" to `strings[1] = strings[1] || "foo"` and the
 // 2nd `strings[1]` is noncompliant:
-strings[1] ||= "foo";
+strings[0] ||= "foo";
 
 
 // no-empty-collection reports an issue here. This is wrong because at
 // this point strings[1] contains "foo":
-if (strings[1]) {
+if (strings[0]) {
   // Same here. Executing this program prints "foo" further confirming
   // that the `if` statement above is correct:
-  console.log(strings[1]);
+  console.log(strings[0]);
 }
